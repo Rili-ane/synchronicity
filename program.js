c3c8e9dad28f38e47c21d1f972987d4ad65ce96c
Rili-ane <38475457+Rili-ane@users.noreply.github.com>
2018-05-17 00:40:50 -0400

    Update program.js

--- program.js
+++ program.js
@@ -1,3 +1,10 @@
+// dem libraries
 const Discord = require("discord.js");
 const ms = require("ms");
 const ontime = require('ontime')
+
+//The bot itself
+const client = new Discord.Client(); //<- IMPORTANT, NEVER REVEAL THE BOT TOKEN
+client.login(process.env.BOT_TOKEN);
+
+if (command == gulag)
