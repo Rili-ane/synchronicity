abc9161660379600547d229270edbfb949f07148
R1NZLER15 <hector15mrc@live.com>
2018-05-18 22:29:40 -0500

    ?

--- program.js
+++ program.js
@@ -63,9 +63,7 @@ client.on("message", async message => {
       return message.reply("Sorry, i cant **gulag** this user");
     let gulag = message.guild.roles.find("name", "gulag");
     let input = message.content.split(" ").slice(1);
-    var time;
-    if(!input)time = `60m`;
-    time = `${input}m`;
+    let time = `${input}m`;
     //let time = `15m`; //params
     member.addRole(gulag).catch(console.error);
     message.channel.send({embed: {
