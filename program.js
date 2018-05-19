85bb4f275d2f3d2e29ce9c127fd43107214d952d
R1NZLER15 <hector15mrc@live.com>
2018-05-18 23:24:09 -0500

    aaa

--- program.js
+++ program.js
@@ -62,10 +62,7 @@ client.on("message", async message => {
     if(member.roles.some(r=>(config.requiredRoles).includes(r.name)) )
       return message.reply("Sorry, i cant **gulag** this user");
     let gulag = message.guild.roles.find("name", "gulag");
-    let input = message.content.split(" ").slice(1)
-      .catch(error => {
-        console.log(error);
-    });
+    let input = message.content.split(" ").slice(1)SS;
     let time = `${input}m`;
     //let time = `1m`;
     //let time = `15m`; //params
