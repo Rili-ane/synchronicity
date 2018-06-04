3b8690353c9f778b82141a025931354937105f0c
R1NZLER15 <hector15mrc@live.com>
2018-06-03 19:23:15 -0500

    fix

--- program.js
+++ program.js
@@ -98,8 +98,8 @@ client.on("message", async message => {
      }, ms(time));
   }
 	
-//water command
-if(command === "water"){
+  //water command
+  if(command === "water"){
     if(!message.member.roles.some(r=>(config.requiredRoles).includes(r.name)) )
       return ;
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
@@ -140,53 +140,3 @@ if(command === "water"){
     message.react('❓')
     });}
 });//End of the on.message event (End of the commands section)
-
-client.on("messageReactionAdd", async reaction => {
-  const message = reaction.message;
-  if (reaction.emoji.name !== '⭐') return;
-  if (message.author.id === user.id) return message.channel.send(`${user}, you cannot star your own messages.`);
-  if (message.author.bot) return message.channel.send(`${user}, you cannot star bot messages.`);
-  const { best_ofChannel } = this.client.settings.get(message.guild.id);
-  const fetch = await message.guild.channels.find('name', 'best-of').fetchMessages({ limit: 100 });
-  const stars = fetch.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(message.id));
-  if (stars) {
-    const star = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
-    const foundStar = stars.embeds[0];
-    const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
-    const embed = new RichEmbed()
-      .setColor(foundStar.color)
-      .setDescription(foundStar.description)
-      .setAuthor(message.author.tag, message.author.displayAvatarURL)
-      .setTimestamp()
-      .setFooter(`⭐ ${parseInt(star[1])+1} | ${message.id}`)
-      .setImage(image);
-    const starMsg = await message.guild.channels.find('name',  best_ofChannel).fetchMessage(stars.id);
-    await starMsg.edit({ embed });
-  }
-  if (!stars) {
-    if (!message.guild.channels.exists('name',  best_ofChannel)) throw `It appears that you do not have a \`${best_of}\` channel.`;
-    const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : '';
-    if (image === '' && message.cleanContent.length < 1) return message.channel.send(`${user}, you cannot star an empty message.`);
-    const embed = new RichEmbed()
-      .setColor(15844367)
-      .setDescription(message.cleanContent)
-      .setAuthor(message.author.tag, message.author.displayAvatarURL)
-      .setTimestamp(new Date())
-      .setFooter(`⭐ 1 | ${message.id}`)
-      .setImage(image);
-    await message.guild.channels.find('name', best_of).send({ embed });
-  }
-});
-
-/*
-* TODO:
-    *Database?
-* hi this tangyuan; can you make it so that the bot removes citizen role when adding gulag role, 
-and when removing gulag role adds citizen role? i tried just adding a member.removeRole(citizen); but citizen was orange for some
-reason aaa
-    *add default timeout for gulag
-    *ungulag command
-    *improve the reddit post's system
-    *suggestion command for the #suggestions channel
-    *meme commands?
-*/
